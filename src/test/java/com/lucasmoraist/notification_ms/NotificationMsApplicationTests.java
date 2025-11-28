f58b7a3ff2f16cb0c69f31e0ae7960d23bdeadf1
lucasmoraist <luksmnt1101@gmail.com>
2025-11-28 14:41:05 -0300

    feat: initial project

---
+++ src/test/java/com/lucasmoraist/notification_ms/NotificationMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasmoraist.notification_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
